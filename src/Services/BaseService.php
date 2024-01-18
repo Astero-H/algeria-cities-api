8e18916f226e697764dd35e4891e75160e149ae4
Astero-H <ahhennane@gmail.com>
2024-01-18 23:05:53 +0100

    Modifying the BaseService methods to perform the search

--- src/Services/BaseService.php
+++ src/Services/BaseService.php
@@ -4,12 +4,34 @@ namespace App\Services;
 
 abstract class BaseService {
 
-    /* Check if the searched city's name is included into one of existing string or if the name match  */
-    public function handleDataList(string $name, array $data, $field = 'name') {
+    public function cleaningExpression($string) {
+        $accents = ['è', 'é', 'ê', 'ë' , 'ï'];
+        $replacements = ['e', 'e', 'e', 'e', 'i'];
+        $replacedString = str_replace($accents, $replacements, $string);
+        $removeSDashes = preg_replace('/-/', '', $replacedString); 
+        $removeApostrophe = preg_replace("/'/", '', $removeSDashes); 
+        $result = str_replace(' ', '', $removeApostrophe);
+        return $result;
+    }
+
+    /* Check if the searched city's name is equal to the string or included into the string  */
+    public function handleDataListWithName(string $name, array $data, string $field = 'name') {
         $fountObjects = [];
-        $nameObj = strtolower($name);
+        $nameObj = strtolower(str_replace(' ', '', $name));
         foreach($data as $dataObj) {
-            if(strcmp(strtolower($dataObj[$field]) , $nameObj) == 0 || strpos(strtolower($dataObj[$field ]), $nameObj) !== false) {                 
+            $dataObjField = $this->cleaningExpression($dataObj[$field]);
+            if(strcmp(strtolower($dataObjField) , $nameObj) == 0 || strpos(strtolower($dataObjField), $nameObj) !== false) {           
+                array_push($fountObjects, $dataObj);
+            } 
+        }
+        return $fountObjects !== [] ? $fountObjects : null ;
+    }
+
+     /* Check if the searched city's name is equal to the string or included into the string  */
+     public function handleDataListWithId(array $data, int $id, string $field ='wilaya_code') {
+        $fountObjects = [];
+        foreach($data as $dataObj) {
+            if ((int)$dataObj[$field] === (int) $id) {         
                 array_push($fountObjects, $dataObj);
             } 
         }
